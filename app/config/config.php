ee3c5ef7352de5255092c4129b86681c358da8f4
Sparrow-33 <elhaoudihaytham2001@gmail.com>
2022-02-18 15:00:59 +0100

    UML and MVC framework added

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // db params
+  define('DB_HOST','localhost');
+  define('DB_USER','root');
+  define('DB_PASS','');
+  define('DB_NAME',' ');
+  // 
+
+  define('APPROOT',dirname(dirname(__FILE__)));
+
+  define('URLROOT','_URL_');
+
+  define('SITENAME','_SATENAME_');
